77a8f30f6187e1adc390ca71ef1029c85548b0ba
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-10-12 14:37:34 -0500

    working on algorithm

--- js/Snakebody.js
+++ js/Snakebody.js
@@ -1,10 +1,44 @@
 // snake body
-
-class SnakeBody {
+const SNAKE_WIDTH = 40;
+const SNAKE_HEIGHT = 40;
+export default class SnakeBody {
   constructor(x, y, parent, child) {
     this.x = x;
     this.y = y;
     this.parent = parent;
     this.child = child;
+    this.pCor = { x: 0, y: 0 };
+  }
+
+  draw(context) {
+    context.fillRect(this.x, this.y, SNAKE_WIDTH, SNAKE_HEIGHT);
+  }
+
+  moveRight(velocity) {
+    this.pCor.x = this.x;
+    this.child.x = this.x;
+    this.x += velocity;
+  }
+
+  moveDown(velocity) {
+    this.pCor.y = this.y;
+    this.y += velocity;
+  }
+
+  moveLeft(velocity) {
+    this.pCor.x = this.x;
+    this.x -= velocity;
+  }
+
+  moveUp(velocity) {
+    this.pCor.y = this.y;
+    this.y -= velocity;
+  }
+
+  changeCor(x, y) {
+    this.pCor.x = this.x;
+    this.pCor.y = this.y;
+    this.x = x;
+    this.y = y;
   }
 }
